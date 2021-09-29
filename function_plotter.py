88e30c698b7fcc684be7f53c20e5bd4b5d6e6492
Amr Ehab <amrabdulaziz98@gmail.com>
2021-09-30 01:13:35 +0200

    reformatting

--- function_plotter.py
+++ function_plotter.py
@@ -1,59 +1,67 @@
 # Import libraries
-from functions import *
-
 from tkinter import ttk
-from ttkthemes import ThemedTk
+
 import matplotlib
+from ttkthemes import ThemedTk
+
+from functions import callback_isdigit, validate
+
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
-
-
 # define window and theme
+
+
 root = ThemedTk(theme="arc")
 style = ttk.Style(root)
-style.configure('Label', background= 'white')
-root.configure(bg= 'white')
+style.configure("Label", background="white")
+root.configure(bg="white")
 root.title("Function Plotter")
 root.resizable(False, False)
 
-reg_isdigit=root.register(callback_isdigit)
-# reg_function=root.register(callback_function)
+reg_isdigit = root.register(callback_isdigit)
 
-#input fields
-function_field = ttk.Entry(root, width = 35)
-# function_field.config(validate="key", validatecommand=(reg_function, '%P'))
+# input fields
+function_field = ttk.Entry(root, width=35)
 
 min_field = ttk.Entry(root)
-min_field.config(validate="key", validatecommand=(reg_isdigit, '%P'))
+min_field.config(validate="key", validatecommand=(reg_isdigit, "%P"))
 
 max_field = ttk.Entry(root)
-max_field.config(validate="key", validatecommand=(reg_isdigit, '%P'))
+max_field.config(validate="key", validatecommand=(reg_isdigit, "%P"))
 
-#labels
-function_field_label = ttk.Label(root, style= 'Label', text = "Enter f(x)\n\t\t\ta*x^b+ c*x^d")
-min_field_label = ttk.Label(root, style= 'Label', text = "Minimum")
-max_field_label = ttk.Label(root, style= 'Label', text = "Maximum")
-    
-plot_btn = ttk.Button(root, text="Plot", command=lambda:validate(function_field, min_field, max_field, subplot, canvas))
+# labels
+function_field_label = ttk.Label(
+    root, style="Label", text="Enter f(x)\n\t\t\ta*x^b + c*x^d"
+)
+min_field_label = ttk.Label(root, style="Label", text="Minimum")
+max_field_label = ttk.Label(root, style="Label", text="Maximum")
 
-#defining the plot
-figure = Figure(figsize = (4,4), dpi = 100)
-subplot = figure.add_subplot(111) 
+plot_btn = ttk.Button(
+    root,
+    text="Plot",
+    command=lambda: validate(function_field, min_field, max_field, subplot, canvas),
+)
+
+# defining the plot
+figure = Figure(figsize=(4, 4), dpi=100)
+subplot = figure.add_subplot(111)
 canvas = FigureCanvasTkAgg(figure)
 
-#pushing everything into the screen
+# pushing everything into the screen
 
-function_field_label.grid(row = 0, column = 0, columnspan = 3, sticky='S'+'W', padx=10 ,pady= 5)
-function_field.grid(row = 1, column = 0, columnspan = 2, padx=10, sticky = 'N')
+function_field_label.grid(
+    row=0, column=0, columnspan=3, sticky="S" + "W", padx=10, pady=5
+)
+function_field.grid(row=1, column=0, columnspan=2, padx=10, sticky="N")
 
-min_field_label.grid(row = 2, column = 0 , sticky = 'N', pady= 5)
-min_field.grid(row = 2, column = 1, sticky = 'N')
-max_field_label.grid(row = 3, column = 0, padx = 3, sticky = 'N', pady= 5)
-max_field.grid(row = 3, column = 1, padx = 3, sticky = 'N')
+min_field_label.grid(row=2, column=0, sticky="N", pady=5)
+min_field.grid(row=2, column=1, sticky="N")
+max_field_label.grid(row=3, column=0, padx=3, sticky="N", pady=5)
+max_field.grid(row=3, column=1, padx=3, sticky="N")
 
-plot_btn.grid(row = 4, columnspan=2, column=0, sticky = 'N')
-canvas.get_tk_widget().grid(row=0, rowspan=5, columnspan=4, column = 4, ipadx=10)
+plot_btn.grid(row=4, columnspan=2, column=0, sticky="N")
+canvas.get_tk_widget().grid(row=0, rowspan=5, columnspan=4, column=4, ipadx=10)
 
 root.mainloop()
--- functions.py
+++ functions.py
@@ -1,14 +1,16 @@
 import numpy as np
 from tkinter import messagebox
 
+
 def callback_isdigit(input):
     if input == "" or input == "-":
         return True
     try:
         float(input)
-    except:
+    except Exception:
         return False
     return True
+
 
 def validate(function_field, min_field, max_field, subplot, canvas):
     y_str = function_field.get()
@@ -16,47 +18,62 @@ def validate(function_field, min_field, max_field, subplot, canvas):
     max_str = max_field.get()
 
     isValid = True
-    if(len(y_str) == 0 or y_str.isspace()):
-        messagebox.showwarning('Missing Field', 'Empty function field')
+    if len(y_str) == 0 or y_str.isspace():
+        messagebox.showwarning("Missing Field", "Empty function field")
         isValid = False
-    elif (len(min_str) == 0):
-        messagebox.showwarning('Missing Field', 'Missing minimum value field')
+    elif len(min_str) == 0:
+        messagebox.showwarning("Missing Field", "Missing minimum value field")
         isValid = False
-    elif (len(max_str) == 0):
-        messagebox.showwarning('Missing Field', 'Missing maximum value field')
+    elif len(max_str) == 0:
+        messagebox.showwarning("Missing Field", "Missing maximum value field")
         isValid = False
-    elif (float(min_str) >= float(max_str)):
-        messagebox.showwarning('Wrong Input','max field is less than min field')
+    elif float(min_str) >= float(max_str):
+        messagebox.showwarning("Wrong Input", "max field should be more than min field")
         isValid = False
 
     for i in y_str:
-        if not i.isdigit() and i not in ['x','*','+','-','/','^',' ','.','(',')']:
-            messagebox.showwarning("Invalid Input", "f(x) can only contain valid characters\n\t\"x,+,-,*,/,^,.,(,)\"")
+        if not i.isdigit() and i not in [
+            "x",
+            "*",
+            "+",
+            "-",
+            "/",
+            "^",
+            " ",
+            ".",
+            "(",
+            ")",
+        ]:
+            messagebox.showwarning(
+                "Invalid Input",
+                'f(x) can only contain valid characters\n\t"x,+,-,*,/,^,.,(,)"',
+            )
             isValid = False
             break
 
     if "**" in y_str:
-        messagebox.showerror('Error','Invalid math expression')
+        messagebox.showerror("Error", "Invalid math expression")
         isValid = False
 
-    if isValid == True:
+    if isValid is True:
         plot(function_field, min_field, max_field, subplot, canvas)
 
+
 def plot(function_field, min_field, max_field, subplot, canvas):
-    y = str(function_field.get()).replace('^', '**')
+    y = str(function_field.get()).replace("^", "**")
     min = float(min_field.get())
     max = float(max_field.get())
     x = np.linspace(min, max, 1000)
 
     try:
         y = eval(y)
-    except:
-        messagebox.showerror('Error','Invalid math expression')
+    except Exception:
+        messagebox.showerror("Error", "Invalid math expression")
 
-    if 'x' not in function_field.get():
+    if "x" not in function_field.get():
         y = np.full(x.shape, y)
 
     subplot.clear()
     subplot.plot(x, y)
-    subplot.ticklabel_format(useMathText=True, axis='both')
+    subplot.ticklabel_format(useMathText=True, axis="both")
     canvas.draw()
